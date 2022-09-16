57276612a4cc3b297a1643aa71fe38b7864220d4
Hanna Arefiardakani <darkshiirarhotmail.com>
2022-09-16 17:44:25 +0200

    for

--- app.js
+++ app.js
@@ -6,21 +6,14 @@ console.log("Testing testing");
 
 
 let fizzBuzz = function(num)
-{
-    if(num % 3 == 0 && num % 5 == 0)
-    {
-        return "FizzBuzz";
-    }
-    else if(num % 3 == 0)
-    {
-        return "Fizz";
-    }
-    else if(num % 5 == 0)
-    {
-        return "Buzz";
-    }
-    else
-    {
-        return num;
+{ for (let i = 1; i <= num; i++)
+    { if (i % 3 === 0 && i % 5 === 0)
+        { console.log("FizzBuzz"); }
+      else if (i % 3 === 0)
+        { console.log("Fizz"); }
+      else if (i % 5 === 0)
+        { console.log("Buzz"); }
+      else
+        { console.log(i); }
     }
 }
