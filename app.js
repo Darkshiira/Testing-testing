ef1495d471853c6c5f4b4897f82d5068adac53bb
Hanna Arefiardakani <darkshiirarhotmail.com>
2022-09-16 17:42:58 +0200

    fizzBuzz

--- app.js
+++ app.js
@@ -3,3 +3,24 @@ console.log("Hej hej");
 let answer = prompt("How's it going? ");
 console.log(`You think its going: ${answer}`);
 console.log("Testing testing");
+
+
+let fizzBuzz = function(num)
+{
+    if(num % 3 == 0 && num % 5 == 0)
+    {
+        return "FizzBuzz";
+    }
+    else if(num % 3 == 0)
+    {
+        return "Fizz";
+    }
+    else if(num % 5 == 0)
+    {
+        return "Buzz";
+    }
+    else
+    {
+        return num;
+    }
+}
