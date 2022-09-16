f6fd25eee72889d585191f52f2ee8297d802658b
Hanna Arefiardakani <darkshiirarhotmail.com>
2022-09-16 17:32:50 +0200

    Merge

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 const prompt = require('prompt-sync')();
-
 console.log("Hej hej");
-
 let answer = prompt("How's it going? ");
-
-console.log("Testing testing");
-
 console.log(`You think its going: ${answer}`);
+console.log("Testing testing");
